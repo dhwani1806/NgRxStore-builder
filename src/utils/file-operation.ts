b8f407e0ea14bbcfd2dd6e26d044526be9182cbf
Dhwani Popat <dhwani.p@simformsolutions.com>
2022-05-07 16:52:19 +0530

    feat: success color added

--- src/utils/file-operation.ts
+++ src/utils/file-operation.ts
@@ -1,9 +1,18 @@
 import * as fs from "fs";
-import  {OUTPUT_LOG_COLORS} from '../utils/constant'
+import { OUTPUT_LOG_COLORS } from "../utils/constant";
 
-export const generateFile = (fileName: string, fileTemplate: string,fileType:string) => {
+export const generateFile = (
+  fileName: string,
+  fileTemplate: string,
+  fileType: string
+) => {
   fs.writeFile(fileName, fileTemplate, function (err) {
     if (err) throw err;
-    console.log(OUTPUT_LOG_COLORS.SUCCESS,`[SUCCESS] ${fileType.charAt(0).toLocaleUpperCase()}${fileType.slice(1)} File Generated!`);
+    console.log(
+      OUTPUT_LOG_COLORS.SUCCESS,
+      `[SUCCESS] ${fileType.charAt(0).toLocaleUpperCase()}${fileType.slice(
+        1
+      )} File Generated!`
+    );
   });
 };
